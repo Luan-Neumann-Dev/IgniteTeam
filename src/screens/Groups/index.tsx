ef703f6895ce3882d591ca515bc3cf7c512b1cef
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-04-15 20:24:41 -0300

    feat: using FlatList to render the GroupCards

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,16 +2,27 @@ import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Galera da RocketSeat', 'Galera do Ignite']);
 
     return (
         <Container>
             <Header/>
             <Highlight title="Turmas" subtitle="Jogue com a sua turma"/>
 
-            <GroupCard title="Galera do Ignite"/>
-            <GroupCard title="Galera do Ignite"/>
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <GroupCard 
+                        title={item} 
+                    /> 
+                )}
+
+            />
         </Container>
     )
 
