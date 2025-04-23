e7077a653832ad416eea9000ba9a3030b74359bf
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2025-04-23 11:28:57 -0300

    chore: treating custom exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,24 +1,36 @@
-import { Header } from "@components/Header";
+import { Alert } from "react-native";
+import { useState } from "react";
 import { Container, Content, Icon } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
+import { AppError } from "@utils/AppError";
 import { groupCreate } from "@storage/group/groupCreate";
 
 
 export function NewGroup() {
     const [group, setGroup] = useState('')
-
     const navigation = useNavigation();
 
     async function handleNew() {
         try {
+            if(group.trim().length === 0) {
+                return Alert.alert("Novo Grupo", 'Informe o nome da turma.')
+            }
+
             await groupCreate(group);
-            navigation.navigate('players', {group})
+            navigation.navigate('players', {group});
+
         } catch (error) {
-            console.log(error);
+            if(error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message)
+            } else {
+                Alert.alert('Novo Grupo', "Não foi possível criar um novo grupo.")
+                console.log(error);
+            }
         }
     }
 
